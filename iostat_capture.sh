c9c210a556bad6b688333e36ce0548ea8d3f7936
Robert Starmer <rstarmer@gmail.com>
2016-03-20 16:39:28 -0700

    fix iostat to 1s intervals

--- iostat_capture.sh
+++ iostat_capture.sh
@@ -3,5 +3,5 @@
 base=$[${0:-0}+1]
 threads=${1:-16}
 disk=`df | awk "/nvme$val$/ {print \$1}"`
-iostat -ytd -p $disk >& /root/iostat-$base-$threads-`date +"%Y-%m-%d-%H-%M"` &
+iostat -ytd -p $disk 1 >& /root/iostat-$base-$threads-`date +"%Y-%m-%d-%H-%M"` &
 echo $! | tee /root/iostat-$base-$threads.pid
