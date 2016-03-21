25d6a214d40b521dd3eecc77c9e66cdf06f92b0d
Robert Starmer <rstarmer@gmail.com>
2016-03-20 21:04:42 -0700

    Address iostat parameter ids

--- iostat_capture.sh
+++ iostat_capture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-base=$[${0:-0}+1]
-threads=${1:-16}
+base=$[${1:-0}+1]
+threads=${2:-16}
 disk=`df | awk "/nvme$val$/ {print \$1}"`
 iostat -ytd -p $disk 1 >& /root/iostat-$base-$threads-`date +"%Y-%m-%d-%H-%M"` &
 echo $! | tee /root/iostat-$base-$threads.pid
