ed354fe03946409938d34d646d2fb57d17fd6993
robertstarmer <robert@kumul.us>
2016-03-31 00:07:23 -0400

    Update to get better csv output from scripts

--- get_rw_ts.sh
+++ get_rw_ts.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-
-#Get timeseries data out of the run data files
-type=$1
-for n in *$1*run
+for file in `ls *.run`
 do
-echo "OP,TIME,OPS" > $n.csv
-grep '\[READ\]\|\[UPDATE\]'  $n >> $n.csv 
+# mongo-1-pkt-nvme-1-128-threads.run
+read db_type db_num target num_dbs threads <<<$( echo $file | sed -e 's/\(.*\)-\([0-9]*\)-\(.*\)-\([0-9]*\)-\([0-9]*\)-threads.run/\1 \2 \3 \4 \5 /') 
+start_date=`grep '0 sec: 0 operations' $file | head -n 1 | sed -e 's/\(.*\):[0-9][0-9][0-9] 0 sec.*/\1/'`
+start_sec=`date --date="$start_date" +"%s"`
+echo "timestamp,OPS,operation_type,db_type,db_number,number_dbs,system_target,threads" > $db_type-$db_num-$target-$num_dbs-$threads.csv
+grep '\[READ\]\|\[UPDATE\]' $file | while read line
+do
+read type sec ops <<<$( echo $line | tr ',' ' ' )
+if [[ $sec =~ ^-?[0-9]+$ ]]; then
+sec_new=$[${sec}/1000]
+time=$[${start_sec}+${sec_new}]
+if [[ "$type" == '[READ]' ]]
+then 
+  type=read
+else
+  type=update
+fi
+new_time=`date --date=\@$time +'%Y%h%d%H%M%S'`
+echo "$new_time,$ops,$type,$db_type,$db_num,$num_dbs,$target,$threads" >> $db_type-$db_num-$target-$num_dbs-$threads.csv 
+fi
+done
 done
--- iostat_clean.sh
+++ iostat_clean.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+num_dbs=${1:-1}
+db_type=${2:-mongo}
+proj=${3:-packet-nvme}
 for n in `ls ./iostat-*`
 do
-  echo $n
-  echo date,time,am,%user,%nice,%system,%iowait,%steal,%idle,device,tps,kB_read/s,kB_wrtn/s,kB_read,kB_wrtn > $n.csv
-  cat $n | grep -v '^Linux\|^$\|avg-cpu\|^Device' | sed -e ':a;$!N;s/\([AP]M\)\n/\1,/;ta;P;D' | sed -e ':a;$!N;s/\([0-9][0-9]\)\n\(nvme.*\)[[:space:]]/\1,\2,/;ta;P;D' | sed -e ':a;$!N;s/\([0-9][0-9]\)\n\(dm-.*\)[[:space:]]/\1,\2,/;ta;P;D' | sed -e 's/[[:space:]]\+/,/g' -e 's/,,/ /g' >> $n.csv
+  read interval db threads <<<$(echo ${n} | sed -e 's/.*iostat-\([0-9]*\)-\([0-9]*\)-\([0-9]*\)-[0-9].*/\1 \2 \3/')
+  echo timestamp,db_type,project,num_dbs,db_num,iostat-int,threads,%user,%nice,%system,%iowait,%steal,%idle,device,rrqm_s,wrqm_s,r_s,w_s,rkB_s,wkB_s,avgrq-sz,avgqu-sz,await,r_await,w_await,svctm,pct_util > $n.csv
+  cat $n | grep -v '^Linux\|^$\|avg-cpu\|^Device' | sed -e ':a;$!N;s/\([AP]M\)\n/\1,/;ta;P;D' | sed -e ':a;$!N;s/\([0-9][0-9]\)\n\(nvme.*\)[[:space:]]/\1,\2,/;ta;P;D' | sed -e ':a;$!N;s/\([0-9][0-9]\)\n\(dm-.*\)[[:space:]]/\1,\2,/;ta;P;D' | sed -e 's/[[:space:]]\+/,/g' -e 's/,,/ /g' | sed -e "s/^\(.*\),\(.*\),\([AP]M\) /\1 \2 \3,$db_type,$proj,$num_dbs,$db,$interval,$threads,/" >> $n.csv
 done
 
