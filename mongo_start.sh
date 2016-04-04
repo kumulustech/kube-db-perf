8188f1ce8df6ec365f1bcdc5ef709930521cc1a8
Robert Starmer <rstarmer@gmail.com>
2016-04-04 14:13:02 -0700

    Update template and start script for more efficent start

--- mongo_start.sh
+++ mongo_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 offset=${2:-2}
-mongo 10.0.0.$[$1*3+20+${offset}] <<EOF
-rs.initiate()
-EOF
+addr=10.0.0.$[$1*3+20+${offset}]
+until [[ `echo "rs.status()" | mongo $addr | tail -n 1` =~ bye ]]; do
+sleep 5
+done
+echo "rs.initiate()" | mongo $addr
